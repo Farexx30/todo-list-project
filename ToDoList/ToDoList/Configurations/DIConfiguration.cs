58dee1a521b700662e7a13676ef11de8795c2926
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-06-16 20:21:36 +0200

    Navigation service added and registered

--- ToDoList/ToDoList/Configurations/DIConfiguration.cs
+++ ToDoList/ToDoList/Configurations/DIConfiguration.cs
@@ -1,17 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Navigation;
 using ToDoList.Models;
+using ToDoList.Services;
 using ToDoList.ViewModels;
-using ToDoList.Views;
 
 namespace ToDoList.Configurations
 {
@@ -28,16 +21,23 @@ namespace ToDoList.Configurations
 
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
-            //Rejestracja DbContext:
+            //Rejestracja DbContextu:
             services.AddDbContext<ToDoListDbContext>(
                 options => options.UseSqlServer(ConfigurationManager.ConnectionStrings["toDoListDatabase"].ConnectionString));
 
+            //Rejestracja serwisów:
+            services.AddSingleton<INavigationService, NavigationService>();
 
+            services.AddSingleton<Func<Type, BaseViewModel>>(provider =>
+                viewModelType => (BaseViewModel)provider.GetRequiredService(viewModelType));
+
+            //Rejestracja MainWindow:
             services.AddSingleton(provider => new MainWindow
             {
-                DataContext = provider.GetRequiredService<MainMenuViewModel>()
+                DataContext = provider.GetRequiredService<MainViewModel>()
             });
-
+            
+            //Rejestracja ViewModelów:
             services.AddTransient<MainMenuViewModel>();
             services.AddTransient<RegisterViewModel>();
             services.AddTransient<LoginViewModel>();
---
+++ ToDoList/ToDoList/Services/NavigationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Services
+{
+    public interface INavigationService
+    {
+        BaseViewModel? CurrentViewModel { get; }
+        void NavigateTo<T>() where T : BaseViewModel;
+    }
+
+    public class NavigationService(Func<Type, BaseViewModel> viewModelFactory) : BaseViewModel, INavigationService
+    {
+         private readonly Func<Type, BaseViewModel> _viewModelFactory = viewModelFactory;
+         private BaseViewModel? _currentViewModel;
+         public BaseViewModel? CurrentViewModel
+         {
+             get => _currentViewModel;
+             private set
+             {
+                 _currentViewModel = value;
+                 OnPropertyChanged();
+             }
+         }
+
+         public void NavigateTo<T>() where T : BaseViewModel
+         {
+             BaseViewModel newViewModel = _viewModelFactory.Invoke(typeof(T));
+
+             CurrentViewModel = newViewModel;
+         }
+    }
+}
