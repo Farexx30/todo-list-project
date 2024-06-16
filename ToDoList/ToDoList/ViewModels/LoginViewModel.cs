91d6dc61dc51e0413dcce7224d1ef3db433f557a
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-06-16 21:37:25 +0200

    Basic navigation done + some adjustments

--- ToDoList/ToDoList/ViewModels/LoginViewModel.cs
+++ ToDoList/ToDoList/ViewModels/LoginViewModel.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Commands;
+using ToDoList.Services;
 
 namespace ToDoList.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private INavigationService _navigationService;
+        public INavigationService NavigationService
+        {
+            get => _navigationService;
+            private set
+            {
+                _navigationService = value;
+                OnPropertyChanged();
+            }
+        }
 
+        public RelayCommand NavigateToMainAppCommand { get; set; } = null!;
+
+        public LoginViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            NavigateToMainAppCommand = new RelayCommand(GoToMainApp, _ => true);
+        }
+
+        private void GoToMainApp(object obj) => _navigationService.NavigateTo<MainAppViewModel>();
     }
 }
