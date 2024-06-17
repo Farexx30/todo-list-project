9c45d1859cab2a25b006306e347b64b403a1f7e9
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-06-17 12:35:53 +0200

    Login Bindings added

--- ToDoList/ToDoList/ViewModels/LoginViewModel.cs
+++ ToDoList/ToDoList/ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using ToDoList.Commands;
 using ToDoList.Services;
 
@@ -21,6 +22,28 @@ namespace ToDoList.ViewModels
             }
         }
 
+        //Bindingi:
+        private string _username = string.Empty;
+        public string Username {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _password = string.Empty;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
         public RelayCommand NavigateToMainAppCommand { get; set; }
 
         public LoginViewModel(INavigationService navigationService)
@@ -30,6 +53,10 @@ namespace ToDoList.ViewModels
             NavigateToMainAppCommand = new RelayCommand(GoToMainApp, _ => true);
         }
 
-        private void GoToMainApp(object obj) => NavigationService.NavigateTo<MainAppViewModel>();
+        private void GoToMainApp(object obj)
+        {
+            MessageBox.Show($"Username: {Username}, Password: {Password}");
+            NavigationService.NavigateTo<MainAppViewModel>();
+        }
     }
 }
